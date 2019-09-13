e0d17b1b6dafc653fb82c555adfa8cfd68a57b79
Ryan Jones <ryan@serverlessguru.com>
2019-09-12 17:41:47 -0700

    [REFACTOR] add comments, clean up functions, add async/await, add response failure/success, utilize AWSLib to clean up aws requests

--- src/handlers/api.js
+++ src/handlers/api.js
@@ -1,76 +1,61 @@
 const uuidv4 = require('uuid/v4');
-const AWSLib = require('../helper/AWSLib')
-const REGION = process.env.REGION;
-AWS.config.update({region: REGION});
+const AWSLib = require('../helper/AWSLib');
+const generic = require('../helper/generic');
 const api = {};
 
-const 
 const TABLE_NAME = process.env.TABLE_NAME;
 
-api.handler = async (event) => {
+/**
+ * Delete user from database by id
+ */
+api.handler = async event => {
   console.log('event', event);
-  let response = {};
-
+  let responseBody = {};
   try {
     if (event.path.includes('users')) {
       if (event.httpMethod === 'POST') {
         // save user in DynamoDB
-        response.body = JSON.stringify(await api.handleCreateUser(JSON.parse(event.body)));
+        responseBody = await api.handleCreateUser(JSON.parse(event.body));
       } else if (event.httpMethod === 'GET') {
         // get user from DynamoDB
-        if (event.pathParameters) {
-          if(event.pathParameters.id) {
-            let userId = event.pathParameters.id;
-            response.body = JSON.stringify(await api.handleGetUserById(userId));
-          }
+        if (event.pathParameters.id) {
+          responseBody = await api.handleGetUserById(event.pathParameters.id);
         } else {
-          console.log('running handleGetUsers');
-          response.body = JSON.stringify(await api.handleGetUsers());
+          responseBody = await api.handleGetUsers();
         }
       } else if (event.httpMethod === 'PUT') {
         // update user in DynamoDB
-        let userId = event.pathParameters.id;
-        response.body = JSON.stringify(await api.handleUpdateUserById(userId, JSON.parse(event.body)));
+        responseBody = await api.handleUpdateUserById(event.pathParameters.id, JSON.parse(event.body));
       } else if (event.httpMethod === 'DELETE') {
         // delete user in DynamoDB
-        let userId = event.pathParameters.id;
-        response.body = JSON.stringify(await api.handleDeleteUserById(userId));
+        responseBody = await api.handleDeleteUserById(event.pathParameters.id);
       }
+    } else {
+      throw {"err": `event.path of ${event.path} not supported`};
     }
-    response.statusCode = 200;
+    return generic.success(responseBody);
   } catch (e) {
-    response.body = JSON.stringify(e);
-    response.statusCode = 500;
+    return generic.failure(e);
   }
-  response.headers = {
-    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-  };
-  console.log('response', response);
-  return response;
 };
 
-api.handleDeleteUserById = (userId) => {
-  return new Promise((resolve, reject) => {
-    let documentClient = new AWS.DynamoDB.DocumentClient();
-
-    let params = {
-      TableName: TABLE_NAME,
-      Key: {
-        user_id: userId
-      }
-    };
-
-    documentClient.delete(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
+/**
+ * Delete user from database by id
+ */
+api.handleDeleteUserById = async userId => {
+  await AWSLib.call('DynamoDB', 'delete', {
+    TableName: TABLE_NAME,
+    Key: { user_id: userId }
   });
+  return { user_id: userId, message: 'User was deleted' };
 };
 
+/**
+ * Update users in the database by id
+ * - stores S3 image
+ * - stores DynamoDB record with S3 image_url
+ */
 api.handleCreateUser = async item => {
-  console.log('item', item);
-
   item.user_id = uuidv4();
 
   if (item.image) {
@@ -97,103 +82,74 @@ api.handleCreateUser = async item => {
     TableName: TABLE_NAME,
     Item: item
   };
-  let ddbResponse = await documentClient.put(ddbParams);
+  let ddbResponse = await AWSLib.call('DynamoDB', 'put', ddbParams);
+  console.log('ddbResponse', ddbResponse);
+
+  return { Item: item };
+};
+
+/**
+ * Update users in the database by id
+ * - stores S3 image
+ * - stores DynamoDB record with S3 image_url
+ */
+api.handleUpdateUserById = async (userId, item) => {
+  if (item.image) {
+    let S3_BUCKET = process.env.S3_BUCKET;
+    let ext = item.image.split(';')[0].split('/')[1];
+    let key = `profile/${item.user_id}/profile.${ext}`;
+    let S3_URL = `https://${S3_BUCKET}.s3-${REGION}.amazonaws.com/${key}`;
+    let buf = new Buffer.from(item.image.replace(/^data:image\/\w+;base64,/, ""),'base64');
+    let s3Params = {
+      ACL: "public-read",
+      Body: buf,
+      Bucket: S3_BUCKET,
+      Key: key,
+      ContentType: `image/${ext}`
+    };
+    let s3Response = await AWSLib.call('S3', 'putObject', s3Params);
+    console.log('s3Response', s3Response);
+
+    delete item.ext;
+    delete item.image;
+    item.image_url = S3_URL;
+  }
+
+  let { updateExpression,
+        expressionAttributeNames,
+        expressionAttributeValues } = await generic.buildDDBUpdate(item);
+
+  let ddbParams = {
+    TableName: TABLE_NAME,
+    Key: {
+      user_id: userId
+    },
+    UpdateExpression: updateExpression,
+    ExpressionAttributeValues: expressionAttributeValues,
+    ExpressionAttributeNames: expressionAttributeNames,
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  console.log('ddbParams', ddbParams);
+
+  let ddbResponse = await AWSLib.call('DynamoDB', 'update', ddbParams);
   console.log('ddbResponse', ddbResponse);
 
   return ddbResponse;
 };
 
-api.handleUpdateUserById = (userId, item) => {
-  return new Promise((resolve, reject) => {
+/**
+ * Get all users from the database
+ */
+api.handleGetUsers = async () => await AWSLib.call('DynamoDB', 'scan', { TableName: TABLE_NAME });
 
-    let s3 = new AWS.S3();
-    let documentClient = new AWS.DynamoDB.DocumentClient();
-
-    if (item.image) {
-      // upload image to S3
-      let S3_BUCKET = process.env.S3_BUCKET;
-      let ext = item.image.split(';')[0].split('/')[1];
-      let key = `profile/${item.user_id}/profile.${ext}`;
-      let S3_URL = `https://${S3_BUCKET}.s3-${REGION}.amazonaws.com/${key}`;
-      let buf = new Buffer.from(item.image.replace(/^data:image\/\w+;base64,/, ""),'base64');
-      let params = {
-        ACL: "public-read",
-        Body: buf,
-        Bucket: S3_BUCKET,
-        Key: key,
-        ContentType: `image/${ext}`
-      };
-      console.log('params', params);
-      s3.putObject(params, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          console.log('s3 data', data);
-          delete item.ext;
-          delete item.image;
-          item.image_url = S3_URL;
-          
-          let params = {
-            TableName: TABLE_NAME,
-            Key: {
-              user_id: userId
-            },
-            UpdateExpression: "set #n = :n, #j = :j, #i = :i",
-            ExpressionAttributeValues: {
-              ":n": item.name,
-              ":j": item.job,
-              ":i": item.image_url
-            },
-            ExpressionAttributeNames: {
-              '#n': 'name',
-              '#j': 'job',
-              '#i': 'image_url'
-            },
-            ReturnValues: "UPDATED_NEW"
-          };
-
-          console.log('ddb params', ddbParams);
-          documentClient.update(params, (err, data) => {
-            if (err) reject(err);
-            else resolve(data);
-          });
-        }
-      });
-    }
-  });
-};
-
-api.handleGetUsers = () => {
-  return new Promise((resolve, reject) => {
-    console.log('--IN api.handleGetUsers()');
-    let documentClient = new AWS.DynamoDB.DocumentClient();
-
-    let params = {
-      TableName: TABLE_NAME
-    };
-
-    documentClient.scan(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
-};
-
-api.handleGetUserById = (userId) => {
-  return new Promise((resolve, reject) => {
-    let documentClient = new AWS.DynamoDB.DocumentClient();
-
-    let params = {
-      TableName: TABLE_NAME,
-      Key: {
-        user_id: userId
-      }
-    };
-
-    documentClient.get(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
+/**
+ * Get users from the database by id
+ */
+api.handleGetUserById = async userId => {
+  return await AWSLib.call('DynamoDB', 'get', {
+    TableName: TABLE_NAME,
+    Key: { user_id: userId }
   });
 };
 
