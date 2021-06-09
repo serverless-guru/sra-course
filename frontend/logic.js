6e2958a5c574b15a9f18d9306e8f7280edbd57fd
Ryan Jones <ryanjones@Ryans-MacBook-Air.local>
2021-06-09 11:23:59 -0400

    add backend/frontend folder and cleanup frontend code

--- frontend/logic.js
+++ frontend/logic.js
@@ -1,106 +1,58 @@
+let url = "https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users";
+
 document.addEventListener('click', function (event) {
-
-	// Don't follow the link
 	event.preventDefault();
-
-    if(event.target.matches('#getUsers')) {
-        document.getElementById('showUsers').innerHTML = '';
-        fetch("https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users", {
-            method: "GET", 
-            // body: JSON.stringify(data)
-        }).then(async res => {
-            let jsonResponse = await res.json()
-            console.log(jsonResponse)
-            let showUsersHTML = "<div>";
-            console.log(jsonResponse.data.Items)
-            if(jsonResponse.data.Count > 0) {
-                jsonResponse.data.Items.forEach(item => {
-                    showUsersHTML += `<h2>${item.name}</h2><p>${item.name} works as a ${item.job}.</p><button class="deleteBtn" name="${item.user_id}">Delete</button>`;
-                });
-            } else {
-                showUsersHTML = "no users found in database"
-            }
-            showUsersHTML += "</div>";
-            document.querySelector('#showUsers').insertAdjacentHTML(
-                'afterbegin',
-                showUsersHTML
-            );
-        });
-    } else if (event.target.matches('#createUser')) {
-        fetch("https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users", {
-            method: "POST", 
-            body: JSON.stringify({
-                name: document.getElementById("nameInput").value,
-                job: document.getElementById("jobInput").value
-            })
-        }).then(async res => {
-            let jsonResponse = await res.json()
-            console.log(jsonResponse)
-            document.querySelector('#showCreateUserMessage').insertAdjacentHTML(
-                'afterbegin',
-                "<p>User has been added</p>"
-            );
-            document.getElementById('showUsers').innerHTML = '';
-            fetch("https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users", {
-                method: "GET", 
-                // body: JSON.stringify(data)
-                }).then(async res => {
-                    let jsonResponse = await res.json()
-                    console.log(jsonResponse)
-                    let showUsersHTML = "<div>";
-                    console.log(jsonResponse.data.Items)
-                    if(jsonResponse.data.Count > 0) {
-                        jsonResponse.data.Items.forEach(item => {
-                            showUsersHTML += `<h2>${item.name}</h2><p>${item.name} works as a ${item.job}.</p><button class="deleteBtn" name="${item.user_id}">Delete</button>`;
-                        });
-                    } else {
-                        showUsersHTML = "no users found in database"
-                    }
-                    showUsersHTML += "</div>";
-                    document.querySelector('#showUsers').insertAdjacentHTML(
-                        'afterbegin',
-                        showUsersHTML
-                    );
-                })
-        }).catch(async res => console.log(await res.json()))
-    } else if (event.target.matches('.deleteBtn')) {
-        console.log(event.target.name)
-        fetch(`https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users/${event.target.name}`, {
-            method: "DELETE"
-        }).then(async res => {
-            let jsonResponse = await res.json()
-            console.log(jsonResponse)
-            document.getElementById('showUsers').innerHTML = '';
-            fetch("https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users", {
-                method: "GET", 
-                // body: JSON.stringify(data)
-                }).then(async res => {
-                    let jsonResponse = await res.json()
-                    console.log(jsonResponse)
-                    let showUsersHTML = "<div>";
-                    console.log(jsonResponse.data.Items)
-                    if(jsonResponse.data.Count > 0) {
-                        jsonResponse.data.Items.forEach(item => {
-                            showUsersHTML += `<h2>${item.name}</h2><p>${item.name} works as a ${item.job}.</p><button class="deleteBtn" name="${item.user_id}">Delete</button>`;
-                        });
-                    } else {
-                        showUsersHTML = "no users found in database"
-                    }
-                    showUsersHTML += "</div>";
-                    document.querySelector('#showUsers').insertAdjacentHTML(
-                        'afterbegin',
-                        showUsersHTML
-                    );
-                })
-        }).catch(async res => console.log(await res.json()))
-    }
+    if(event.target.matches('#getUsers')) fetchUsers()
+    else if (event.target.matches('#createUser')) createUser()
+    else if (event.target.matches('.deleteBtn')) deleteUser(event.target.name)
 }, false);
 
+function fetchUsers () {
+    document.getElementById('showUsers').innerHTML = '';
+    fetch(url, {
+        method: "GET",
+    }).then(async res => {
+        let jsonResponse = await res.json()
+        console.log(jsonResponse)
+        let showUsersHTML = "<div>";
+        console.log(jsonResponse.data.Items)
+        if(jsonResponse.data.Count > 0) {
+            jsonResponse.data.Items.forEach(item => {
+                showUsersHTML += `<h2>${item.name}</h2><p>${item.name} works as a ${item.job}.</p><button class="deleteBtn" name="${item.user_id}">Delete</button>`;
+            });
+        } else {
+            showUsersHTML = "no users found in database"
+        }
+        showUsersHTML += "</div>";
+        document.querySelector('#showUsers').insertAdjacentHTML(
+            'afterbegin',
+            showUsersHTML
+        );
+    });
+}
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", "https://q39q2b06b0.execute-api.us-west-2.amazonaws.com/dev/users", true);
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.send(JSON.stringify({
-//     name: "ryan",
-//     job: "dev"
-// }));
+function createUser () {
+    fetch(url, {
+        method: "POST", 
+        body: JSON.stringify({
+            name: document.getElementById("nameInput").value,
+            job: document.getElementById("jobInput").value
+        })
+    }).then(async res => {
+        let jsonResponse = await res.json()
+        console.log(jsonResponse)
+        document.querySelector('#showCreateUserMessage').insertAdjacentHTML(
+            'afterbegin',
+            "<p>User has been added</p>"
+        );
+        fetchUsers()
+    }).catch(async res => console.log(await res.json()))
+}
+
+function deleteUser (userId) {
+    fetch(`${url}/${userId}`, {
+        method: "DELETE"
+    })
+    .then(res => fetchUsers())
+    .catch(async res => console.log(await res.json()))
+}
